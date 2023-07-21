2c7bcdf1ed845c6646fb0538b53a8a989dc29add
ndmservis <139966533+ndmservis@users.noreply.github.com>
2023-07-21 11:40:12 +0800

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -12,9 +12,9 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 clear
-echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
-echo "Please input link to your vps data backup file."
-echo "You can check it on your email if you run backup data vps before."
+echo "Fitur Ini Hanya Bisa Digunakan Sesuai Data Vps Dengan Autoscript Ini."
+echo "Silahkan masukan link ke file backup data vps anda."
+echo "Anda dapat memeriksanya di email Anda jika Anda menjalankan backup data vps sebelumnya."
 read -rp "Link File: " -e url
 wget -O backup.zip "$url"
 unzip backup.zip
