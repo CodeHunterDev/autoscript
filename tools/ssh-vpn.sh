f842dbc7539f28fbcba8c72cc1caf06016814ab2
ndmservis <139966533+ndmservis@users.noreply.github.com>
2023-07-21 13:44:58 +0800

    Update ssh-vpn.sh

--- tools/ssh-vpn.sh
+++ tools/ssh-vpn.sh
@@ -233,9 +233,9 @@ echo -e "[ ${green}INFO$NC ] Disable ipv6"
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6 >/dev/null 2>&1
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local >/dev/null 2>&1
 
-# set time GMT +8
+# set time GMT +7
 sleep 1
-echo -e "[ ${green}INFO$NC ] Set zona local time to Asia/singapura GMT+8"
+echo -e "[ ${green}INFO$NC ] Set zona local time to Asia/Jakarta GMT+7"
 ln -fs /usr/share/zoneinfo/Asia/singapura /etc/localtime
 
 # set locale
